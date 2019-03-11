2ac560e800db64e62dbdab71a6d0e7fc83ac9286
Hadi-94 <Mohammedhadi_15@hotmail.com>
2019-03-12 00:38:34 +0800

    Adjustment on Authentication 3

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -11,14 +11,14 @@ import { AlertController } from '@ionic/angular';
 })
 export class RegisterPage {
 
-    username: string = ""
+    email: string = ""
     password: string = ""
     conPassword: string = ""
 
     constructor(private router: Router, public afAuth: AngularFireAuth, public alertController: AlertController) { }
 
     async goToRegisterPage() {
-        const { username, password, conPassword } = this
+        const { email, password, conPassword } = this
 
         if (password !== conPassword) {
             const alert = await this.alertController.create({
@@ -29,15 +29,38 @@ export class RegisterPage {
             await alert.present();
         }
 
-        try {
-            const res = await this.afAuth.auth.createUserWithEmailAndPassword(username, password)
-            this.router.navigateByUrl('/students')
-        } catch (err) {
-            console.dir(err)
+        else {
+                try {
+                const res = await this.afAuth.auth.createUserWithEmailAndPassword(email, password)
+                this.router.navigateByUrl('/students')
+            } catch (err) {
+                console.dir(err)
+                if (err.code == "auth/weak-password"){
+                    const alert = await this.alertController.create({
+                        header: 'Alert',
+                        message: 'The password must be 6 characters long or more',
+                        buttons: ['OK']
+                    });
+                    await alert.present();
+                }
+                else if (err.code == "auth/invalid-email") {
+                    const alert = await this.alertController.create({
+                        header: 'Alert',
+                        message: 'Invalid Email',
+                        buttons: ['OK']
+                    });
+                await alert.present();
+                }
+                else if (err.code == "auth/email-already-in-use") {
+                    const alert = await this.alertController.create({
+                        header: 'Alert',
+                        message: 'The email address is already in use by another account',
+                        buttons: ['OK']
+                    });
+                await alert.present();
+                }
+            }
         }
     }
+} 
 
-
-  
-
-}
